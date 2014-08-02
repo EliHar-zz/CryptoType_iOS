9d3710730e56523d0a117eb62932f4b2635ef9d1
Elias Haroun <dev.elihar@gmail.com>
2014-08-01 20:18:31 -0400

    safe commit

---
+++ Crypto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Crypto
+//
+//  Created by Elias Haroun on 2014-06-20.
+//  Copyright (c) 2014 Elias Haroun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CryptoTests/CryptoTests.m
@@ -0,0 +1,34 @@
+//
+//  CryptoTests.m
+//  CryptoTests
+//
+//  Created by Elias Haroun on 2014-06-20.
+//  Copyright (c) 2014 Elias Haroun. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CryptoTests : XCTestCase
+
+@end
+
+@implementation CryptoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
